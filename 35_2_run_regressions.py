9be311a0b6c0f9bb8d94e86deb94b12d13c6b78b
cara <cara.p.lynch@gmail.com>
2024-03-14 21:28:39 +0000

    warnings

--- 35_2_run_regressions.py
+++ 35_2_run_regressions.py
@@ -18,7 +18,7 @@ from multiprocessing import Pool
 # custom imports
 from regression_class import RedditRegression as RR
 
-OUTDIR = "regression_test_outputs"
+OUTDIR = "regression_outputs"
 # PARAMS_DICT_INFILE = f"{OUTDIR}/input_params.p"
 REGRESSION_INFILE = "regression_thread_data.p"
 THREAD_INFILE = "clean_5_thread_data.p"
@@ -33,7 +33,7 @@ OUTFILE = f"{OUTDIR}/regressions.p"
 def run_regression(params_dict):
     try:
         name = f"{params_dict['name']}_{params_dict['regression_type']}"
-        logger.info(f"  ### {name} ###")
+        logger.info(f"  {name}")
         # logfile handler
         sub_f_handler = logging.FileHandler(f"{LOGDIR}/{name}.log")
         sub_f_handler.setLevel(logging.INFO)
@@ -43,18 +43,18 @@ def run_regression(params_dict):
         regression = RR(params_dict, log_handlers=handlers)
         regression.main()
     except Exception as e:
-        logger.exception(f"Exception occurred in {name} process")
+        logger.exception(f" Exception occurred in {name} process")
     finally:
         out_dict[name] = regression
-        logger.info(f"  ### {name} FINISHED ###")
+        logger.info(f"  {name} FINISHED")
 
 
 if __name__ == "__main__":
     # warnings.simplefilter("ignore")
+    #warnings.filterwarnings("ignore", category=RuntimeWarning)
     start = dt.now()
     logger = logging.getLogger(__name__)
     logger.setLevel(logging.INFO)
-    logger.info(f"Start time {start}, PID {os.getpid()}")
 
     # stream handler for logging
     c_handler = logging.StreamHandler()
@@ -79,6 +79,7 @@ if __name__ == "__main__":
     f_handler.setFormatter(f_format)
     logger.addHandler(f_handler)
 
+    logger.info(f"Start time {start}, PID {os.getpid()}")
     out_dict = {}
 
     try:
@@ -86,7 +87,7 @@ if __name__ == "__main__":
         logger.info("   Regression infile")
         regression_df = pickle.load(open(REGRESSION_INFILE, "rb"))
         logger.info("   Thread infile")
-        thread_df = pickle.load(open(THREAD_INFILE, "wb"))
+        thread_df = pickle.load(open(THREAD_INFILE, "rb"))
         logger.info("## Finished reading in files ##")
 
         # params_dicts = pickle.load(open(PARAMS_DICT_INFILE, "rb"))
